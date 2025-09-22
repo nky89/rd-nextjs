8c4a7f1107fd994c2106d7254d5850b6c5167a01
Kevin Niu <nky.wifi+code@gmail.com>
2025-09-23 00:46:13 +0800

    Finish Chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -123,3 +123,27 @@ export type State = {
     };
     message?: string | null;
 };
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+// ...
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
